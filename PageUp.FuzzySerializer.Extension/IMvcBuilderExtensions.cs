733e9de6752f678fc3258bc52ddab4086e4dcac4
Abhaya Chauhan <abschauhan@gmail.com>
2017-08-27 14:50:11 +1000

    Added Extension method or IMvcBuilder

---
+++ PageUp.FuzzySerializer.Extension/IMvcBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PageUp.FuzzySerializer.Extension 
+{
+    public static class IMvcBuilderExtensions {
+
+        public static IMvcBuilder AddFuzzySerializer(this IMvcBuilder mvcBuilder) {
+            return mvcBuilder.AddJsonOptions(opt =>
+                {
+                    opt.SerializerSettings.ContractResolver = new FuzzyObjectContractResolver();
+                });
+        }
+
+        public static IMvcBuilder AddFuzzySerializer(this IMvcBuilder mvcBuilder, FuzzyObjectContractResolverSettings fuzzySettings) {
+            return mvcBuilder.AddJsonOptions(opt =>
+                {
+                    opt.SerializerSettings.ContractResolver = new FuzzyObjectContractResolver(fuzzySettings);
+                });
+        }
+        
+    }
+}
