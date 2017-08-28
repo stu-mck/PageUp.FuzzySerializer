cb605b2e47ce6ce94c45cccbf69c9ce59ad15012
QUEEN\abhayac <QUEEN\abhayac@pageuppeople.com>
2017-08-28 17:24:37 +1000

    NetFramework Extensions

---
+++ PageUp.FuzzySerializer.NetFrameworkExtension/HttpConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace PageUp.FuzzySerializer.NetFrameworkExtension
+{
+    public static class HttpConfigurationExtensions
+    {
+        public static void AddFuzzyResponses(this HttpConfiguration configuration, FuzzyObjectContractResolverSettings settings)
+        {
+            configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new FuzzyObjectContractResolver(settings);
+        }
+
+        public static void AddFuzzyResponses(this HttpConfiguration configuration)
+        {
+            configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new FuzzyObjectContractResolver();
+        }
+    }
+}
